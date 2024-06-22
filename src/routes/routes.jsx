8f30f8cb76eb1bb2f9febac37d099dee60301999
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-06-22 15:46:52 +0500

    fixed bugs and added payment forms

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,20 +1,20 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "../pages/home";
-import Dashboard from "../pages/dashboard";
-function Routes () {
-    return (
-        <RouterProvider
-        router={createBrowserRouter([
-          {
-            path: "/",
-            element: <Home/>,
-          },
-          {
-            path: "dashboard",
-            element: <Dashboard/>,
-          },
-        ])}
-      />
-    )
+import Home from "@/pages/home";
+import Dashboard from "@/pages/dashboard";
+function Routes() {
+  return (
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+        },
+      ])}
+    />
+  );
 }
 export default Routes;
